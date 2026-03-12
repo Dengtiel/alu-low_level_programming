33e5ae8d3c5d0f3e16b0221e22bd791a63bab71e
Dengtiel <d.akol@alustudent.com>
2026-03-12 13:54:05 +0200

    Add dog struct definition

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+# C - Structures, typedef
+
+This directory contains C programming exercises on structures and typedef.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's basic information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Structure to store dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
