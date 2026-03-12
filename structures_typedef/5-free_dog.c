90b3c6c07cf331261df92f4f98de4c0ccd8945ab
Dengtiel <d.akol@alustudent.com>
2026-03-12 14:06:09 +0200

    Add function to free dog structure

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees a dog structure
+ * @d: Pointer to dog to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
