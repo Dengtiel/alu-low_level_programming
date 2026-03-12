a17935b49109b1d2496f7d8e4283c302b7bd699e
Dengtiel <d.akol@alustudent.com>
2026-03-12 13:44:06 +0200

    Add recursive prime number checker

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * check_prime - Helper function to check if number is prime
+ * @n: Number to check
+ * @divisor: Current divisor to test
+ *
+ * Return: 1 if prime, 0 if not prime
+ */
+int check_prime(int n, int divisor)
+{
+	if (divisor * divisor > n)
+	{
+		return (1);
+	}
+
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+
+	return (check_prime(n, divisor + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime
+ * @n: Number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	return (check_prime(n, 2));
+}
